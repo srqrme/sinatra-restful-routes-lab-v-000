c89df9f47557f231c690d39db9dac8c085b4b0d9
Sharmaine Requerme <srqrme@gmail.com>
2019-01-11 19:27:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get 'recipes/:id/edit' do
+  get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
 
     erb :edit
