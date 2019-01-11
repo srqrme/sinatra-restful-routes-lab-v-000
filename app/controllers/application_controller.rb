34c408a7778599bf2112e9f860492d9d107997ce
Sharmaine Requerme <srqrme@gmail.com>
2019-01-11 18:39:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,9 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  post'/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
 end
